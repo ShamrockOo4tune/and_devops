76f4dc9bfe7e304309e475d88a1d972f84f1dcb4
Shamil Gumerov <shamusg12345@gmail.com>
2021-11-21 13:06:03 +0300

    lesson2 hw: add inventory, ansible.cfg, flaskapp.py

---
+++ lesson2/flaskapp.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from socket import gethostname
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    hostname = gethostname()
+    return f'This is the simple flask application identifying the hostname={hostname} of the node on which it is running'
+
+if __name__=="__main__":
+    app.run()
+
